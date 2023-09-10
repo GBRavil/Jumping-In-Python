fd12bd0ef397b4177a121d252de355375055522b
GBRavil <114304656+GBRavil@users.noreply.github.com>
2023-09-10 15:51:56 +0300

    Create ex_01.py

---
+++ S5/ex_01.py
@@ -0,0 +1,11 @@
+# Написать функцию, которая принимает на вход строку — абсолютный путь до файла.
+# Функция возвращает кортеж из трёх элементов: путь, имя файла, расширение файла.
+
+def get_tuple(path):
+    *path_el, name_ext = path.split('/')
+    name, extension = name_ext.split('.')
+    return '/'.join(path_el) + '/', name, extension
+
+all_path = '/Users/kalishka82/PyCharmProjects/GB_Jumping_In_Python/ht_04_functions/ex_01.py'
+
+print(get_tuple(all_path))
